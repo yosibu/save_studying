9e82f40ea4d4b17cdf563af869230e8a374e3450
i18yositake <yosibu@outlook.jp>
2022-03-06 23:05:55 +0900

    change

--- static/main.js
+++ static/main.js
@@ -1,24 +1,24 @@
 let count = 0;
 let intervalID;
 s_btn.onclick = async () => {
-  document.getElementById("w_btn").disabled =true;
-  document.getElementById("f_btn").disabled =true;
-  document.getElementById("s_btn").disabled =false;
-/*   intervalID = setInterval(action, 1000); */
+  document.getElementById("w_btn").disabled =false;
+  document.getElementById("f_btn").disabled =false;
+  document.getElementById("s_btn").disabled =true;
+  intervalID = setInterval(action, 1000);
 };
 
 w_btn.onclick = async () => {
   clearInterval(intervalID);
-  document.getElementById("s_btn").disabled =true;
-  document.getElementById("w_btn").disabled =false;
+  document.getElementById("s_btn").disabled =false;
+  document.getElementById("w_btn").disabled =true;
 }
 
 f_btn.onclick = async () => {
   clearInterval(intervalID);
   count = 0;
-  document.getElementById("s_btn").disabled =true;
-  document.getElementById("w_btn").disabled =false;
-  document.getElementById("f_btn").disabled =false;
+  document.getElementById("s_btn").disabled =flase;
+  document.getElementById("w_btn").disabled =true;
+  document.getElementById("f_btn").disabled =true;
 }
 
 function action(){

7cb43e1fd8d094def4b43f4c8c05854da9bf922c
i18yositake <yosibu@outlook.jp>
2022-03-07 02:15:03 +0900

    change

--- main.tsx
+++ main.tsx
@@ -1,9 +1,9 @@
 
 import { serve } from "https://deno.land/std@0.127.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.127.0/http/file_server.ts";
-/* 
+
 const databaseUrl = Deno.env.get("DATABASE_URL")!;
-const pool = new postgres.Pool(databaseUrl, 3, true);
+/* const pool = new postgres.Pool(databaseUrl, 3, true);
 
 const connection = await pool.connect();
 try {

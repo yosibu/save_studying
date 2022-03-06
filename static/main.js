52dcc3104412c76007fdc3a8599cf4d6f5c237d9
i18yositake <yosibu@outlook.jp>
2022-03-07 00:04:43 +0900

    change

--- static/main.js
+++ static/main.js
@@ -14,11 +14,11 @@ w_btn.onclick = async () => {
 }
 
 f_btn.onclick = async () => {
-  clearInterval(intervalID);
-  count = 0;
   document.getElementById("s_btn").disabled =flase;
   document.getElementById("w_btn").disabled =true;
   document.getElementById("f_btn").disabled =true;
+  clearInterval(intervalID);
+  count = 0;
 }
 
 function action(){

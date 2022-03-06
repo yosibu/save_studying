48427c93f35e60b2379919ef33c24ea688e13ab4
i18yositake <yosibu@outlook.jp>
2022-03-07 00:12:14 +0900

    change

--- static/main.js
+++ static/main.js
@@ -24,8 +24,8 @@ f_btn.onclick = async () => {
 function action(){
     count++;
 
-    let H = count / 3600;
-    let M = count / 60;
+    let H = Math.floor(count % 60000 / 1000);
+    let M = Math.floor(count % 3600000 / 60000);
     let S = count % 60;
     let h = ('00' + H).slice(-2);
     let m = ('00' + M).slice(-2);

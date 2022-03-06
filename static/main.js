7747a888c789e1c5183321e964930b3bcc0945dc
i18yositake <yosibu@outlook.jp>
2022-03-06 21:29:40 +0900

    change

--- static/main.js
+++ static/main.js
@@ -1,20 +1,8 @@
-var Person = /** @class */ (function () {
-    function Person(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    Person.prototype.sayHello = function () {
-        return "My name is ".concat(this.name, ". I'm ").concat(this.age, " years old.");
-    };
-    return Person;
-}());
-var p = new Person('Takumi', 19);
-var s = p.sayHello();
-document.getElementById('div1').innerHTML = s;
-
 let count = 0;
+
 btn.onclick = async () => {
   setInterval(action, 1000);
+  btn.style.display = "none";
 };
 
 function action(){

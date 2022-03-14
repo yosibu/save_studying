410ae206284ec5c2af9ff2ff1eea738e03cde59b
i18yositake <yosibu@outlook.jp>
2022-03-15 00:14:46 +0900

    change

--- static/main.js
+++ static/main.js
@@ -65,7 +65,7 @@ function addTable(niti,jika){
 
 function addData(kiroku,days,kai){
   let table = document.getElementById("kirokuTable")
-  table.tbody.deleteRow(0);
+/*   table.tbody.deleteRow(0); */
   for(let i = 0;i < kai; i++){
     addTable(days[i],kiroku[i]);
   }
